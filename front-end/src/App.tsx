d831d18b3751bc57ee4a9d3b3ce553cead45ebe2
sim-ware <samir.gossain@gmail.com>
2021-06-13 08:42:24 +0100

    Adds speedy dropdown from material UI for eventual front-end filters

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,27 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   const [filters, setFilters] = useState(null);
   const [chartInfo, setChartInfo] = useState(null);
+  const [age, setAge] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:8000/getAllIdNameAndYearRanges')
@@ -16,11 +33,28 @@ function App() {
       .then(data => setChartInfo(data));
   }, []);
 
+  const handleChange = (event: any) => {
+    setAge(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="Filter-list-container">
           <p>Filter List</p>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
           <p>{JSON.stringify(filters)}</p>
         </div>
         <div className="Visual-data-container">
